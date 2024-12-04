b43d70f34d01ac098782c5dcac4c5ab40795b54c
Hauke <hank.la.boon@gmail.com>
2024-12-04 12:53:03 +0100

    add navigation

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,15 +4,19 @@ import styled from "styled-components";
 export default function Layout({ children }) {
   return (
     <>
-      <h1>Art Gallery</h1>
+      <StyledH1>Art Gallery</StyledH1>
       <div>{children}</div>
       <StyledFooter>
-        <button>
-          <Link href="/">Spotlight</Link>
-        </button>
-        <button>
-          <Link href="/art-overview">Gallery</Link>
-        </button>
+        <StyledButton>
+          <StyledLink href="/">
+            <strong>Spotlight</strong>
+          </StyledLink>
+        </StyledButton>
+        <StyledButton>
+          <StyledLink href="/art-overview">
+            <strong>Gallery</strong>
+          </StyledLink>
+        </StyledButton>
       </StyledFooter>
     </>
   );
@@ -22,4 +26,30 @@ const StyledFooter = styled.footer`
   display: flex;
   background-color: black;
   width: 100%;
+  position: fixed;
+  bottom: 0px;
+  justify-content: space-evenly;
+`;
+
+const StyledButton = styled.button`
+  flex: 1;
+  cursor: pointer;
+  border: 4px solid #1f1e1c;
+  background-color: #0d1117;
+  padding: 1em 0;
+  /* &:hover {
+    background-color: grey; */
+  /* } */
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 2rem;
+  color: white;
+  &:hover {
+    background-color: #0d1117da;
+  }
+`;
+const StyledH1 = styled.h1`
+  text-align: center;
 `;
