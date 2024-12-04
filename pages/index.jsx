09169f387765238c64fb59c90bbf67dadc218f33
Dennis <d-l-b@gmx.de>
2024-12-04 14:34:37 +0100

    added dynamic routing

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,6 @@
 import Card from "@/components/Card";
 
 export default function HomePage({ artistArray }) {
-  console.log("home page array:", artistArray);
-
   function getRandomArt() {
     const randomIndex = Math.floor(Math.random() * artistArray.length);
     return artistArray[randomIndex];
